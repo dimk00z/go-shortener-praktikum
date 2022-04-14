c621e5bfbe948ec150f46d513c36e0ecc1627e7f
Dimk00z <dimk00z@gmail.com>
2022-04-14 20:01:30 +0300

    Update server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/dimk00z/go-shortener-praktikum/internal/handlers"
 )
 
 func main() {
-	// маршрутизация запросов обработчику
-	http.HandleFunc("/", handlers.HelloWorld)
-	// запуск сервера с адресом localhost, порт 8080
-	http.ListenAndServe(":8080", nil)
+	handler1 := handlers.MyHandler{
+		Templ: []byte("Hola, Mundo"),
+	}
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: handler1,
+	}
+	log.Fatal(server.ListenAndServe())
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,3 +5,11 @@ import "net/http"
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Hello, World</h1>"))
 }
+
+type MyHandler struct {
+	Templ []byte
+}
+
+func (h MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write(h.Templ)
+}
