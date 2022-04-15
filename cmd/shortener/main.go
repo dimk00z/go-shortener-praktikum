1a0a1d124b71565ce9ac2175740cb83dfb8eec85
Dimk00z <dimk00z@gmail.com>
2022-04-15 09:38:54 +0300

    Update learning handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/dimk00z/go-shortener-praktikum/internal/handlers"
 )
 
 func main() {
 	handler1 := handlers.MyHandler{
-		Templ: []byte("Hola, Mundo"),
+		// Templ: []byte("Hola, Mundo"),
 	}
 
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: handler1,
-	}
-	log.Fatal(server.ListenAndServe())
+	mux := http.NewServeMux()
+	mux.Handle("/", handler1)
+
+	nameHandler := handlers.HameHandler{}
+	mux.Handle("/hello/", nameHandler)
+
+	th := handlers.TimeHandler{Format: time.RFC1123}
+	mux.Handle("/time", th)
+	mux.Handle("/ya", http.RedirectHandler("https://ya.ru/", 301))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
