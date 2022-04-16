3a3c9a608611cce7f87a1a8da06a1e2bb440b91c
Dimk00z <dimk00z@gmail.com>
2022-04-16 14:54:34 +0300

    Refactor handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -12,7 +12,7 @@ import (
 )
 
 type RootHandler struct {
-	storage storage.URLsStorage
+	storage storage.URLStorage
 	host    string
 }
 
@@ -23,46 +23,49 @@ func NewRootHandler(host string) *RootHandler {
 	}
 }
 
+func (h RootHandler) HandleGETRequest(w http.ResponseWriter, r *http.Request) {
+	shortURL := strings.Replace(r.URL.Path, "/", "", 1)
+	var err error
+	shortURL, err = h.storage.GetByShortURL(shortURL)
+	if err != nil {
+		log.Println(shortURL, err)
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	} else {
+		w.Header().Set("Location", shortURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+}
+
+func (h RootHandler) HandlePOSTRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Body == http.NoBody {
+		http.Error(w, "Request should have body", http.StatusBadRequest)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	URL := string(body)
+	if !util.IsURL(URL) {
+		http.Error(w, "Wrong URL given", http.StatusBadRequest)
+		return
+	}
+	shortURL := h.storage.SaveURL(URL)
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("%s/%s", h.host, shortURL)))
+}
+
 func (h RootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		{
-			shortURL := strings.Replace(r.URL.Path, "/", "", 1)
-			var err error
-			shortURL, err = h.storage.GetByShortURL(shortURL)
-			if err != nil {
-				log.Println(shortURL, err)
-				http.Error(w, err.Error(), http.StatusNotFound)
-				return
-			} else {
-				w.Header().Set("Location", shortURL)
-				w.WriteHeader(http.StatusTemporaryRedirect)
-				return
-			}
-		}
+		h.HandleGETRequest(w, r)
 	case "POST":
-		{
-			if r.Body == http.NoBody {
-				http.Error(w, "Request should have body", http.StatusBadRequest)
-				return
-			}
-
-			body, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-			URL := string(body)
-			if !util.IsURL(URL) {
-				http.Error(w, "Wrong URL given", http.StatusBadRequest)
-				return
-			}
-			shortURL := h.storage.SaveURL(URL)
-
-			w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte(fmt.Sprintf("%s/%s", h.host, shortURL)))
-
-		}
+		h.HandlePOSTRequest(w, r)
 	}
 }
