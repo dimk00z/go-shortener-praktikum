93fd6ef409b125627b08655583a8856d5da0297b
Dimk00z <dimk00z@gmail.com>
2022-05-02 10:55:47 +0300

    Add increment 6(flag)

--- internal/settings/settings.go
+++ internal/settings/settings.go
@@ -1,6 +1,7 @@
 package settings
 
 import (
+	"flag"
 	"log"
 
 	"github.com/caarlos0/env"
@@ -21,25 +22,38 @@ type Config struct {
 	Storage StorageConfig
 }
 
+func flagGiven(fl string) bool {
+	return flag.Lookup(fl) == nil
+}
+
+func (c *Config) checkFlags() {
+	if flagGiven("a") {
+		flag.StringVar(&c.Server.Port, "a", c.Server.Port, "SERVER_ADDRESS")
+	}
+	if flagGiven("b") {
+		flag.StringVar(&c.Server.Host, "b", c.Server.Port, "BASE_URL")
+	}
+	if flagGiven("f") {
+		flag.StringVar(&c.Storage.FileStorage.FilePath, "f", c.Storage.FileStorage.FilePath, "FILE_STORAGE_PATH")
+	}
+	flag.Parse()
+}
+
 var currentConfig *Config
 
 func LoadConfig() *Config {
 	if currentConfig != nil {
 		return currentConfig
 	}
-	cfg := Config{}
-	serverConfig := ServerConfig{}
-	if err := env.Parse(&serverConfig); err != nil {
+	cfg := new(Config)
+	if err := env.Parse(&cfg.Server); err != nil {
 		log.Printf("%+v\n", err)
 	}
-	cfg.Server = serverConfig
-	var fileStorage FileStorageConfig
-	if err := env.Parse(&fileStorage); err != nil {
+	if err := env.Parse(&cfg.Storage.FileStorage); err != nil {
 		log.Printf("%+v\n", err)
 	}
-	cfg.Storage = StorageConfig{
-		FileStorage: fileStorage,
-	}
-	return &cfg
+	cfg.checkFlags()
+	currentConfig = cfg
+	return currentConfig
 
 }
