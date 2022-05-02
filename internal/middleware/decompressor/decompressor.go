fb73d848a3387c91d78c37ee4af8c6c14aed7e94
Dimk00z <dimk00z@gmail.com>
2022-05-02 17:12:52 +0300

    Fix codestyle

--- internal/middleware/decompressor/decompressor.go
+++ internal/middleware/decompressor/decompressor.go
@@ -8,17 +8,18 @@ import (
 
 func DecompressHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
-			gz, err := gzip.NewReader(r.Body)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			defer gz.Close()
-			r.Body = gz
-		}
+		if !strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			next.ServeHTTP(w, r)
+			return
 
+		}
+		gz, err := gzip.NewReader(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer gz.Close()
+		r.Body = gz
 		next.ServeHTTP(w, r)
-		return
 	})
 }
