fbe62dd6974293e305735441ff71227b954816e2
Dimk00z <dimk00z@gmail.com>
2022-06-06 17:25:52 +0300

    Add worker struct

---
+++ internal/worker/worker.go
@@ -0,0 +1,53 @@
+package worker
+
+import (
+	"context"
+	"log"
+	"sync"
+)
+
+type WorkersPool struct {
+	workersNumber int
+	inputCh       chan func(ctx context.Context) error
+}
+
+func NewWorkersPool(workersNumber int, poolLegth int) *WorkersPool {
+	return &WorkersPool{
+		workersNumber: workersNumber,
+		inputCh:       make(chan func(ctx context.Context) error, poolLegth),
+	}
+}
+
+func (wp *WorkersPool) Push(task func(ctx context.Context) error) {
+	wp.inputCh <- task
+}
+
+func doWorkersTask(ctx context.Context,
+	workerIndex int,
+	wg *sync.WaitGroup,
+	taskCh chan func(ctx context.Context) error) {
+	log.Printf("worker %v started\n", workerIndex)
+workerLoop:
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("worker %v got context.Done\n", workerIndex)
+			break workerLoop
+		case workerTask := <-taskCh:
+			if err := workerTask(ctx); err != nil {
+				log.Printf("worker %v got error:%s", workerIndex, err.Error())
+			}
+		}
+	}
+	wg.Done()
+}
+
+func (wp *WorkersPool) Run(ctx context.Context) {
+	wg := &sync.WaitGroup{}
+	for workerIndex := 0; workerIndex < wp.workersNumber; workerIndex++ {
+		wg.Add(1)
+		go doWorkersTask(ctx, workerIndex, wg, wp.inputCh)
+	}
+	wg.Wait()
+	close(wp.inputCh)
+}
